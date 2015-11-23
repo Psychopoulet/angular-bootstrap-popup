0ba8e78e96dda5689bf81a17bf84eceb688bfb07
Sébastien VIDAL <svidal@wynd.eu>
2015-11-23 17:16:03 +0100

    ajout du dictionnaire, et des fonctions de prompt et de confirm

--- angular-bootstrap-popup.js
+++ angular-bootstrap-popup.js
@@ -1,123 +1,261 @@
 angular.module('ngBootstrapPopup', []).service('$popup', function() {
 
-    "use strict";
+	"use strict";
 
-    // methodes
+	// attributes
 
-        // private
+		// public
 
-            function _create(p_stPopupOptions, p_stModaleOptions) {
+			this.lng = {
+				titles : {
+					alert : 'Alert',
+					confirm : 'Confirm',
+					prompt : 'Prompt',
+					preview : 'Preview'
+				},
+				buttons : {
+					ok : 'Ok',
+					close : 'Close',
+					no : 'No',
+					yes : 'Yes'
+				}
+			};
 
-                var clModal, clDialog, clContent, clHeader, clBody, clFooter, clButtonClose;
+		// private
 
-                p_stPopupOptions = (p_stPopupOptions) ? p_stPopupOptions : {};
-                p_stModaleOptions = (p_stModaleOptions) ? p_stModaleOptions : {};
+			var that = this, m_nCountPrompt = 0;
 
-                clModal = jQuery('<div class="modal fade text-left" data-backdrop="static" data-keyboard="true" data-show="false"></div>');
+	// methodes
 
-                    clDialog = jQuery('<div class="modal-dialog modal-' + ((p_stPopupOptions.size && 'large' == p_stPopupOptions.size) ? 'lg' : 'sm') + ' modal-vertical-centered"></div>');
+		// private
 
-                        clContent = jQuery('<div class="modal-content"></div>');
+			function _create(p_stPopupOptions, p_stModaleOptions) {
 
-                            clHeader = jQuery('<div class="modal-header"><h4 class="modal-title"></h4></div>');
+				var clModal, clDialog, clContent, clHeader, clBody, clFooter, clButtonClose;
 
-                                if (p_stPopupOptions.title && 0 < p_stPopupOptions.title.length) {
+				p_stPopupOptions = (p_stPopupOptions) ? p_stPopupOptions : {};
+				p_stModaleOptions = (p_stModaleOptions) ? p_stModaleOptions : {};
 
-                                    if (1 == p_stPopupOptions.title.length) {
-                                        clHeader.text(p_stPopupOptions.title.toUpperCase());
-                                    }
-                                    else {
-                                        clHeader.text(p_stPopupOptions.title.charAt(0).toUpperCase() + p_stPopupOptions.title.slice(1));
-                                    }
+				clModal = jQuery('<div class="modal fade text-left"></div>');
 
-                                }
-                                else if (p_stPopupOptions.title_html) {
-                                    clHeader.html(p_stPopupOptions.title_html);
-                                }
-                                else {
-                                    clHeader.text('Alert');
-                                }
+					clDialog = jQuery('<div class="modal-dialog modal-' + ((p_stPopupOptions.size && 'large' == p_stPopupOptions.size) ? 'lg' : 'sm') + ' modal-vertical-centered"></div>');
 
-                            clContent.append(clHeader);
+						clContent = jQuery('<div class="modal-content"></div>');
 
-                            clBody = jQuery('<div class="modal-body"></div>');
+							clHeader = jQuery('<div class="modal-header"><h4 class="modal-title"></h4></div>');
 
-                                if (p_stPopupOptions.content && 0 < p_stPopupOptions.content.length) {
+								if (p_stPopupOptions.title && 0 < p_stPopupOptions.title.length) {
 
-                                    if (1 == p_stPopupOptions.content.length) {
-                                        clBody.text(p_stPopupOptions.content.toUpperCase());
-                                    }
-                                    else {
-                                        clBody.text(p_stPopupOptions.content.charAt(0).toUpperCase() + p_stPopupOptions.content.slice(1));
-                                    }
+									if (1 == p_stPopupOptions.title.length) {
+										clHeader.text(p_stPopupOptions.title.toUpperCase());
+									}
+									else {
+										clHeader.text(p_stPopupOptions.title.charAt(0).toUpperCase() + p_stPopupOptions.title.slice(1));
+									}
 
-                                }
-                                else if (p_stPopupOptions.content_html) {
-                                    clBody.html(p_stPopupOptions.content_html);
-                                }
-                                else {
-                                    clBody.text('Alert');
-                                }
+								}
+								else if (p_stPopupOptions.title_html) {
+									clHeader.html(p_stPopupOptions.title_html);
+								}
+								else {
+									clHeader.text(that.lng.titles.alert);
+								}
 
-                            clContent.append(clBody);
+							clContent.append(clHeader);
 
-                                clButtonClose = jQuery('<button type="button" class="btn btn-primary">Ok</button>');
+							clBody = jQuery('<div class="modal-body"></div>');
 
-                                    clButtonClose.click(function () {
-                                        clModal.modal('hide');
-                                    });
+								if (p_stPopupOptions.content && 0 < p_stPopupOptions.content.length) {
 
-                                clFooter = jQuery('<div class="modal-footer"></div>').append(clButtonClose);
+									if (1 == p_stPopupOptions.content.length) {
+										clBody.text(p_stPopupOptions.content.toUpperCase());
+									}
+									else {
+										clBody.text(p_stPopupOptions.content.charAt(0).toUpperCase() + p_stPopupOptions.content.slice(1));
+									}
 
-                            clContent.append(clFooter);
+								}
+								else if (p_stPopupOptions.contentHTML) {
+									clBody.html(p_stPopupOptions.contentHTML);
+								}
+								else {
+									clBody.text(that.lng.titles.alert);
+								}
 
-                        clDialog.append(clContent);
+							clContent.append(clBody);
 
-                    clModal.append(clDialog);
+								if (p_stPopupOptions.buttons && 0 < p_stPopupOptions.buttons.length) {
 
-                    p_stModaleOptions.show = true;
+									clFooter = jQuery('<div class="modal-footer"></div>');
 
-                clModal.modal(p_stModaleOptions);
+									angular.forEach(p_stPopupOptions.buttons, function(stButton) {
 
-                clModal
-                    .on('hidden.bs.modal', function () {
-                        clModal.remove();
-                        clModal = null;
-                    });
+										var clButton = jQuery('<button type="button" class="btn btn-default"></button>');
 
-                return clModal;
+											if (stButton.cls) {
+												clButton.addClass(stButton.cls);
+											}
 
-            }
+											if (stButton.text) {
+												clButton.append(stButton.text);
+											}
 
-        // public
+											if (stButton.click) {
 
-            this.alert = function (p_sMessage, p_sTitle) {
+												if (!angular.isArray(stButton.click)) {
+													stButton.click = [ stButton.click ];
+												}
 
-                return _create({
-                    title : (p_sTitle) ? p_sTitle : 'Alert',
-                    content_html : p_sMessage
-                }, {
-                    backdrop : 'static',
-                    keyboard : true
-                });
+												clButton.click(function() {
 
-            };
+													angular.forEach(stButton.click, function(fClick) {
+														
+														if ('function' === typeof fClick) {
+															fClick();
+														}
+														else if ('close' == fClick) {
+															clModal.modal('hide');
+														}
 
-            this.preview = function (p_sUrl, p_sTitle) {
+													});
 
-                return _create({
-                    title : (p_sTitle) ? p_sTitle : 'Preview',
-                    content_html : '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="' + p_sUrl + '" frameborder="0" allowfullscreen></iframe></div>',
-                    size : 'large'
-                }, {
-                    backdrop : 'static',
-                    keyboard : true
-                });
+												});
 
-            };
+											}
 
-            this.closeAll = function () {
-                jQuery('.modal').modal('hide');
-            };
+										clFooter.append(clButton);
+
+									});
+
+								}
+
+							clContent.append(clFooter);
+
+						clDialog.append(clContent);
+
+					clModal.append(clDialog);
+
+					p_stModaleOptions.show = true;
+
+				clModal.modal(p_stModaleOptions);
+
+				clModal
+					.on('shown.bs.modal', function () {
+
+						if (p_stPopupOptions.shown && 'function' === typeof p_stPopupOptions.shown) {
+							p_stPopupOptions.shown();
+						}
+
+					})
+					.on('hidden.bs.modal', function () {
+						clModal.remove();
+						clModal = null;
+					});
+
+				return clModal;
+
+			}
+
+		// public
+
+			this.alert = function (p_sMessage, p_sTitle) {
+
+				return _create({
+					title : (p_sTitle) ? p_sTitle : that.lng.titles.alert,
+					contentHTML : p_sMessage,
+					buttons : [
+						{
+							text : that.lng.buttons.ok,
+							click : 'close'
+						}
+					]
+				},
+				{
+					backdrop : 'static',
+					keyboard : true
+				});
+
+			};
+
+			this.confirm = function (p_sMessage, p_sTitle, p_fOnYes, p_fOnNo) {
+
+				return _create({
+					title : (p_sTitle) ? p_sTitle : that.lng.titles.confirm,
+					contentHTML : p_sMessage,
+					buttons : [
+						{
+							cls : 'btn-primary',
+							text : that.lng.buttons.yes,
+							click : ('function' === typeof p_fOnYes) ? [ p_fOnYes, 'close' ] : 'close'
+						},
+						{
+							text : that.lng.buttons.no,
+							click : ('function' === typeof p_fOnNo) ? [ p_fOnNo, 'close' ] : 'close'
+						}
+					]
+				},
+				{
+					backdrop : 'static',
+					keyboard : true
+				});
+
+			};
+
+			this.prompt = function (p_sTitle, p_fOnConfirm, p_fOnAbort) {
+
+				p_sTitle = (p_sTitle) ? p_sTitle : that.lng.titles.prompt;
+
+				++m_nCountPrompt;
+
+				var sId = 'idPopupFormPrompt' + m_nCountPrompt;
+
+				return _create({
+					title : p_sTitle,
+					contentHTML : '<div class="form-group"><label for="' + sId + '">' + p_sTitle + '</label><input id="' + sId + '" type="text" class="form-control" /></div>',
+					buttons : [
+						{
+							cls : 'btn-primary',
+							text : that.lng.buttons.ok,
+							click : ('function' === typeof p_fOnConfirm) ? [ function() { p_fOnConfirm(jQuery('#' + sId).val()); }, 'close' ] : 'close'
+						},
+						{
+							text : that.lng.buttons.close,
+							click : ('function' === typeof p_fOnAbort) ? [ p_fOnAbort, 'close' ] : 'close'
+						}
+					],
+					shown : function () {
+						jQuery('#' + sId).focus()
+					}
+				},
+				{
+					backdrop : 'static',
+					keyboard : true
+				});
+
+			};
+
+			this.preview = function (p_sUrl, p_sTitle) {
+
+				return _create({
+					title : (p_sTitle) ? p_sTitle : that.lng.titles.preview,
+					contentHTML : '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="' + p_sUrl + '" frameborder="0" allowfullscreen></iframe></div>',
+					size : 'large',
+					buttons : [
+						{
+							text : that.lng.buttons.close,
+							click : 'close'
+						}
+					]
+				},
+				{
+					backdrop : 'static',
+					keyboard : true
+				});
+
+			};
+
+			this.closeAll = function () {
+				jQuery('.modal').modal('hide');
+			};
 
 });
